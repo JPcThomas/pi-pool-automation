50537dc08cbaec243f42e923634b6075b4a33449
jpcthomas <jonpaulcthomas@gmail.com>
2025-04-02 06:21:00 +0100

    need to figure bug out

--- public/app.js
+++ public/app.js
@@ -9,6 +9,12 @@ const relay8 = document.getElementById('relay8');
 
 relay1.addEventListener('click', turnOnAux1);
 relay2.addEventListener('click', turnOnAux2);
+relay3.addEventListener('click', turnOnAux3);
+relay4.addEventListener('click', turnOnAux4);
+relay5.addEventListener('click', turnOnAux5);
+relay6.addEventListener('click', turnOnAux6);
+relay7.addEventListener('click', turnOnAux7);
+relay8.addEventListener('click', turnOnAux8);
 
 async function turnOnAux1() {
     const response = await fetch('http://192.168.0.90:3000/aux1', {method: 'POST'})
@@ -18,6 +24,42 @@ async function turnOnAux1() {
 
 async function turnOnAux2(){
     const response = await fetch('http://192.168.0.90:3000/aux2', {method: 'POST'})
+    const data = await response.text();
+    console.log('Backend Response:', data);
+}
+
+async function turnOnAux3(){
+    const response = await fetch('http://192.168.0.90:3000/aux3', {method: 'POST'})
+    const data = await response.text();
+    console.log('Backend Response:', data);
+}
+
+async function turnOnAux4(){
+    const response = await fetch('http://192.168.0.90:3000/aux4', {method: 'POST'})
+    const data = await response.text();
+    console.log('Backend Response:', data);
+}
+
+async function turnOnAux5(){
+    const response = await fetch('http://192.168.0.90:3000/aux5', {method: 'POST'})
+    const data = await response.text();
+    console.log('Backend Response:', data);
+}
+
+async function turnOnAux6(){
+    const response = await fetch('http://192.168.0.90:3000/aux6', {method: 'POST'})
+    const data = await response.text();
+    console.log('Backend Response:', data);
+}
+
+async function turnOnAux7(){
+    const response = await fetch('http://192.168.0.90:3000/aux7', {method: 'POST'})
+    const data = await response.text();
+    console.log('Backend Response:', data);
+}
+
+async function turnOnAux8(){
+    const response = await fetch('http://192.168.0.90:3000/aux8', {method: 'POST'})
     const data = await response.text();
     console.log('Backend Response:', data);
 }
--- server.js
+++ server.js
@@ -3,28 +3,34 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const relay1 = new Gpio(529, 'out')
-const relay2 = new Gpio(539, 'out')
-const relay3 = new Gpio(534, 'out')
-const relay4 = new Gpio(535, 'out')
-const relay5 = new Gpio(536, 'out')
-const relay6 = new Gpio(528, 'out')
-const relay7 = new Gpio(532, 'out')
-const relay8 = new Gpio(533, 'out')
+const relay1 = new Gpio(529, 'out');
+const relay2 = new Gpio(539, 'out');
+const relay3 = new Gpio(534, 'out');
+const relay4 = new Gpio(535, 'out');
+const relay5 = new Gpio(536, 'out');
+const relay6 = new Gpio(528, 'out');
+const relay7 = new Gpio(532, 'out');
+const relay8 = new Gpio(533, 'out');
 
-relay1.write(1);
-relay2.write(1)
-relay3.write(1)
-relay4.write(1)
-relay5.write(1)
-relay6.write(1)
-relay7.write(1)
-relay8.write(1)
+relay1.writeSync(1);
+relay2.writeSync(1);
+relay3.writeSync(1);
+relay4.writeSync(1);
+relay5.writeSync(1);
+relay6.writeSync(1);
+relay7.writeSync(1);
+relay8.writeSync(1);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 let isAux1On = false;
 let isAux2On = false;
+let isAux3On = false;
+let isAux4On = false;
+let isAux5On = false;
+let isAux6On = false;
+let isAux7On = false;
+let isAux8On = false;
 
 app.post('/aux1', (req, res) =>{
     console.log("recieved request at aux1");
@@ -39,7 +45,7 @@ app.post('/aux1', (req, res) =>{
     res.send(isAux1On?"Aux1 is On":"Aux1 is off");
 })
 app.post('/aux2', (req, res) =>{
-    console.log("recieved request at aux1");
+    console.log("recieved request at aux2");
     isAux2On = !isAux2On;
         if(isAux2On){
             relay2.writeSync(0);
@@ -48,9 +54,68 @@ app.post('/aux2', (req, res) =>{
             relay2.writeSync(1);
         }
             
-    res.send(isAux2On?"Aux1 is On":"Aux1 is off");
+    res.send(isAux2On?"Aux2 is On":"Aux2 is off");
 })
-
+app.post('/aux3', (req, res) =>{
+    console.log("recieved request at aux3");
+    isAux3On = !isAux3On;
+        if(isAux3On){
+            relay3.writeSync(0);
+        }
+        else{
+            relay3.writeSync(1);
+        }
+})
+app.post('/aux4', (req, res) =>{
+    console.log("recieved request at aux4");
+    isAux4On = !isAux4On;
+        if(isAux4On){
+            relay4.writeSync(0);
+        }
+        else{
+            relay4.writeSync(1);
+        }
+})
+app.post('/aux5', (req, res) =>{
+    console.log("recieved request at aux5");
+    isAux5On = !isAux5On;
+        if(isAux5On){
+            relay5.writeSync(0);
+        }
+        else{
+            relay5.writeSync(1);
+        }
+})
+app.post('/aux6', (req, res) =>{
+    console.log("recieved request at aux6");
+    isAux6On = !isAux6On;
+        if(isAux6On){
+            relay6.writeSync(0);
+        }
+        else{
+            relay6.writeSync(1);
+        }
+})
+app.post('/aux7', (req, res) =>{
+    console.log("recieved request at aux7");
+    isAux7On = !isAux7On;
+        if(isAux7On){
+            relay7.writeSync(0);
+        }
+        else{
+            relay7.writeSync(1);
+        }
+})
+app.post('/aux8', (req, res) =>{
+    console.log("recieved request at aux8");
+    isAux8On = !isAux8On;
+        if(isAux8On){
+            relay8.writeSync(0);
+        }
+        else{
+            relay8.writeSync(1);
+        }
+})
 app.listen(port, () =>{
     console.log(`Server is running at http://localhost:${port})`)
 });
